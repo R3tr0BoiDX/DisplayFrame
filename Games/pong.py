9a643e661ad8b4be6ca7426e8eb3eb0d2a365507
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2023-01-09 00:47:36 +0100

    :rolling_eyes:

--- Games/pong.py
+++ Games/pong.py
@@ -64,12 +64,12 @@ def main():
         if ball_pos_x < matrix.LED_WIDTH - offset_horizontal:
             ball_pos_x += 1
         else:
-            ball_pos_x -= 1
+            ball_pos_x *= -1
 
         if ball_pos_y < matrix.LED_HEIGHT - offset_vertical:
             ball_pos_y += 1
         else:
-            ball_pos_y -= 1
+            ball_pos_y *= -1
 
         display.show()
         time.sleep(CLOCK_SPEED)
