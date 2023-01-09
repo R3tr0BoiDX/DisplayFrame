88da91008fc2557158779032f2d6769c89bb2051
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2023-01-09 03:51:02 +0100

    :shrug:

--- Games/pong.py
+++ Games/pong.py
@@ -73,7 +73,7 @@ def main():
 
     # wait for either player to press start
     display.show()
-    while not bit_ops.check_bit(input_p1.current_input, 64):
+    while not bit_ops.check_bit(input_p1.current_input, 6):
         print(input_p1.current_input)
         time.sleep(0.5)
 
