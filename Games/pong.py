2ca43836bf48b1ac888dcf49c75bf7713614df78
Janisch, Mirco <mirco.janisch@stud.h-da.de>
2023-01-09 04:15:47 +0100

    :shrug:

--- Games/pong.py
+++ Games/pong.py
@@ -65,6 +65,8 @@ def main():
     p1_points = 0
     p2_points = 0
 
+    points_pos_min_y = 6
+
     # todo duplicate
     matrix.set_pixel((ball_pos_x, ball_pos_y), WHITE, display)
     for i in range(0, player_height):
@@ -82,71 +84,90 @@ def main():
 
     # main loop
     game_over = False
+    round_over = False
     while not game_over:
-        matrix.flush(display)
+        while not round_over:
+            matrix.flush(display)
 
-        # draw ball
-        matrix.set_pixel((ball_pos_x, ball_pos_y), WHITE, display)
+            # draw ball
+            matrix.set_pixel((ball_pos_x, ball_pos_y), WHITE, display)
 
-        # check collision and switch direction horizontally
-        if ball_pos_x > matrix.LED_WIDTH - ball_offset_horizontal - 1 or ball_pos_x < ball_offset_horizontal + 1:
+            # check collision, give points and switch direction horizontally
+            if ball_pos_x > matrix.LED_WIDTH - ball_offset_horizontal - 1 or ball_pos_x < ball_offset_horizontal + 1:
 
-            missed = True
-            if ball_dir_x < 0:
-                for i in range(0, player_height):
-                    if ball_pos_y == p1_pos_y + i:
-                        ball_dir_x *= -1
-                        missed = False
+                missed = True
+                if ball_dir_x < 0:
+                    for i in range(0, player_height):
+                        if ball_pos_y == p1_pos_y + i:
+                            ball_dir_x *= -1
+                            missed = False
 
-                if missed:
-                    p2_points += 1
-                    print("point for p2")
+                    if missed:
+                        p2_points += 1
 
-            else:
-                for i in range(0, player_height):
-                    if ball_pos_y == p1_pos_y + i:  # todo: player two
-                        ball_dir_x *= -1
-                        missed = False
+                else:
+                    for i in range(0, player_height):
+                        if ball_pos_y == p1_pos_y + i:  # todo: player two
+                            ball_dir_x *= -1
+                            missed = False
 
-                if missed:
-                    p1_points += 1
-                    print("point for p1")
+                    if missed:
+                        p1_points += 1
 
-            game_over = missed
+                round_over = missed
 
-        # switch direction vertically
-        if ball_pos_y > matrix.LED_HEIGHT - ball_offset_vertical - 2 or ball_pos_y < ball_offset_vertical + 2:
-            ball_dir_y *= -1
+            # switch direction vertically
+            if ball_pos_y > matrix.LED_HEIGHT - ball_offset_vertical - 2 or ball_pos_y < ball_offset_vertical + 2:
+                ball_dir_y *= -1
 
-        # move ball
-        ball_pos_x += (1 * ball_dir_x)
-        ball_pos_y += (1 * ball_dir_y)
+            # move ball
+            ball_pos_x += (1 * ball_dir_x)
+            ball_pos_y += (1 * ball_dir_y)
 
-        # player one
-        if bit_ops.check_bit(input_p1.current_input, 0):  # up
-            input_p1.current_input = bit_ops.clear_bit(input_p1.current_input, 0)
-            if p1_pos_y > ball_offset_vertical:
-                p1_pos_y -= 1
+            # player one
+            if bit_ops.check_bit(input_p1.current_input, 0):  # up
+                input_p1.current_input = bit_ops.clear_bit(input_p1.current_input, 0)
+                if p1_pos_y > ball_offset_vertical:
+                    p1_pos_y -= 1
 
-        if bit_ops.check_bit(input_p1.current_input, 1):  # down
-            input_p1.current_input = bit_ops.clear_bit(input_p1.current_input, 1)
-            if p1_pos_y < matrix.LED_HEIGHT - ball_offset_vertical - player_height:
-                p1_pos_y += 1
+            if bit_ops.check_bit(input_p1.current_input, 1):  # down
+                input_p1.current_input = bit_ops.clear_bit(input_p1.current_input, 1)
+                if p1_pos_y < matrix.LED_HEIGHT - ball_offset_vertical - player_height:
+                    p1_pos_y += 1
 
-        for i in range(0, player_height):
-            matrix.set_pixel((player_offset_x, p1_pos_y + i), WHITE, display)
+            for i in range(0, player_height):
+                matrix.set_pixel((player_offset_x, p1_pos_y + i), WHITE, display)
 
-        # todo: player two
-        for i in range(0, player_height):
-            matrix.set_pixel((matrix.LED_WIDTH - player_offset_x, p1_pos_y + i), WHITE, display)
+            # todo: player two
+            for i in range(0, player_height):
+                matrix.set_pixel((matrix.LED_WIDTH - player_offset_x, p1_pos_y + i), WHITE, display)
 
-        # game logic
+            # game logic
+            display.show()
+            time.sleep(CLOCK_SPEED)
+
+        # round over. reset and start next round
+        matrix.blink(1, BLINK_DELAY_GAME_OVER, display)
+
+        # todo: redudant
+        ball_pos_x = matrix.LED_WIDTH // 2
+        ball_pos_y = matrix.LED_HEIGHT // 2
+        ball_dir_x = random.choice([-1, 1])
+        ball_dir_y = random.choice([-1, 1])
+        p1_pos_y = matrix.LED_HEIGHT // 2
+
+        for i in range(p1_points):
+            matrix.set_pixel((0, points_pos_min_y - i), WHITE, display)
         display.show()
-        time.sleep(CLOCK_SPEED)
+        round_over = False
 
-    matrix.blink(BLINK_COUNT_GAME_OVER, BLINK_DELAY_GAME_OVER, display)
-    matrix.clear(display)
     print("game over")
+    matrix.blink(BLINK_COUNT_GAME_OVER, BLINK_DELAY_GAME_OVER, display)
+
+    # wait for either player to press start, todo: redudant
+    while not bit_ops.check_bit(input_p1.current_input, 6) and not bit_ops.check_bit(input_p2.current_input, 6):
+        time.sleep(0.5)
+    matrix.clear(display)
 
 
 if __name__ == '__main__':
